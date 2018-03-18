8d9b2423b214443380690c3dbf9f583e4099ca57
xaviarmengol <xafontova@gmail.com>
2018-03-18 23:18:40 +0100

    Versió 0 de la web funcionant

--- MevaFlaskApp.py
+++ MevaFlaskApp.py
@@ -1,13 +1,25 @@
+import pandas as pd
 from flask import Flask, render_template
+#from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
+#Bootstrap (app)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def cover():
+    return render_template('cover.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+s
